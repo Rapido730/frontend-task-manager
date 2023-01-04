d7caef72827d98618c00edf06bbca4f7bb583c1d
Rapido730 <sharmamit510@gmail.com>
2023-01-04 23:32:46 +0530

    minor changes

--- src/store/tasks/tasks.reducer.js
+++ src/store/tasks/tasks.reducer.js
@@ -1,44 +1,7 @@
 import { Tasks_Action_Types } from "./tasks.types";
-const data = [
-  {
-    _id: "63b1cc2c4398095c222245e4",
-    description: "washing",
-    completed: true,
-    author: "63aee3e95295796bdb016c16",
-    createdAt: "2023-01-01T18:08:44.979Z",
-    updatedAt: "2023-01-01T18:42:10.411Z",
-    __v: 0,
-  },
-  {
-    _id: "63b1d484238d61535ebb6c9a",
-    description: "bathing",
-    completed: false,
-    author: "63aee3e95295796bdb016c16",
-    createdAt: "2023-01-01T18:44:20.013Z",
-    updatedAt: "2023-01-01T18:44:20.013Z",
-    __v: 0,
-  },
-  {
-    _id: "63b1d48f238d61535ebb6c9d",
-    description: "cleaning",
-    completed: false,
-    author: "63aee3e95295796bdb016c16",
-    createdAt: "2023-01-01T18:44:31.247Z",
-    updatedAt: "2023-01-01T18:44:31.247Z",
-    __v: 0,
-  },
-  {
-    _id: "63b1d49b238d61535ebb6ca0",
-    description: "charging",
-    completed: false,
-    author: "63aee3e95295796bdb016c16",
-    createdAt: "2023-01-01T18:44:43.372Z",
-    updatedAt: "2023-01-01T18:44:43.372Z",
-    __v: 0,
-  },
-];
+
 const INITIAL_STATE = {
-  tasks: data,
+  tasks: [],
 };
 
 export const TasksReducer = (state = INITIAL_STATE, action) => {
